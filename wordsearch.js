effbfd8f926e0184fdf932ad78eb7abbf068f303
SorrenJ <soalexjao@gmail.com>
2024-04-18 18:44:18 -0700

    stretch activity

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,53 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    const results = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      results[i] = [];
+      
+      for (let j = 0; j < matrix.length; j++) {
+        results[i][j] = matrix[j][i];
+      }
     }
+    return results;
+  };
+
+  const internalJoining = function(array) {
+    const horizontalJoin = array.map(ls => ls.join(''));
+    return horizontalJoin;
+  }
+
+  const internalSearch = function(arr, word) {
+    let joinedArr = internalJoining(arr);
+    for (l of joinedArr) {
+        if (l.includes(word)) return true
+    } 
+  }
+
+  
+
+const wordSearch = (letters, word) => { 
+    
+    if (internalSearch(letters, word)) {
+        return true;
+    }
+
+    const transLetters = transpose(letters);
+    if (internalSearch(transLetters, word)) {
+        return true;
+    }
+    // const transHorizontalJoin = transLetters.map((ls) => ls.join(""));
+    // for (l of transHorizontalJoin) {
+    //   if (l.includes(word)) return true;
+    // }
+    return false;
 }
 
+
+const backwards1 = function(array) {
+    const hori = array.map(ls => ls.join(''));
+    return horizontalJoin;
+  }
+
+
 module.exports = wordSearch
+
+//printMatrix(transpose([[1, 2, 3, 4, 5, 6, 7]]));
